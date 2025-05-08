7bb5f51495f6fd2b71e94e3a07b511b553675938
WinehouseD <kitdiana6@gmail.com>
2025-05-08 19:57:34 +0300

    modify animation logic to avoid glitching

--- scroll.js
+++ scroll.js
@@ -2,43 +2,12 @@ gsap.registerPlugin(ScrollTrigger);
 
 let iteration = 0;
 
-gsap.set(".cards li", { xPercent: 400, opacity: 0, scale: 0 });
-
 const spacing = 0.1,
-  snapTime = gsap.utils.snap(spacing),
+  snap = gsap.utils.snap(spacing),
   cards = gsap.utils.toArray(".cards li"),
-  animateFunc = (element) => {
-    const tl = gsap.timeline();
-    tl.fromTo(
-      element,
-      { scale: 0.3, opacity: 0.7 },
-      {
-        scale: 1.1,
-        opacity: 1,
-        zIndex: 100,
-        rotateY: 0,
-        duration: 0.5,
-        yoyo: true,
-        repeat: 1,
-        ease: "power1.in",
-        immediateRender: false,
-      }
-    ).fromTo(
-      element,
-      { xPercent: 400 },
-      { xPercent: -400, duration: 1, ease: "none", immediateRender: false },
-      0
-    );
-    return tl;
-  },
-  seamlessLoop = buildSeamlessLoop(cards, spacing, animateFunc),
-  playhead = { offset: 0 },
-  wrapTime = gsap.utils.wrap(0, seamlessLoop.duration()),
-  scrub = gsap.to(playhead, {
-    offset: 0,
-    onUpdate() {
-      seamlessLoop.time(wrapTime(playhead.offset));
-    },
+  seamlessLoop = buildSeamlessLoop(cards, spacing),
+  scrub = gsap.to(seamlessLoop, {
+    totalTime: 0,
     duration: 0.5,
     ease: "power3",
     paused: true,
@@ -46,78 +15,107 @@ const spacing = 0.1,
   trigger = ScrollTrigger.create({
     start: 0,
     onUpdate(self) {
-      let scroll = self.scroll();
-      if (scroll > self.end - 1) {
-        wrap(1, 2);
-      } else if (scroll < 1 && self.direction < 0) {
-        wrap(-1, self.end - 2);
+      if (self.progress === 1 && self.direction > 0 && !self.wrapping) {
+        wrapForward(self);
+      } else if (self.progress < 1e-5 && self.direction < 0 && !self.wrapping) {
+        wrapBackward(self);
       } else {
-        scrub.vars.offset =
-          (iteration + self.progress) * seamlessLoop.duration();
+        scrub.vars.totalTime = snap(
+          (iteration + self.progress) * seamlessLoop.duration()
+        );
         scrub.invalidate().restart();
+        self.wrapping = false;
       }
     },
     end: "+=3000",
     markers: false,
     pin: ".gallery",
-  }),
-  progressToScroll = (progress) =>
-    gsap.utils.clamp(
-      1,
-      trigger.end - 1,
-      gsap.utils.wrap(0, 1, progress) * trigger.end
-    ),
-  wrap = (iterationDelta, scrollTo) => {
-    iteration += iterationDelta;
-    trigger.scroll(scrollTo);
-    trigger.update();
-  };
+  });
 
-ScrollTrigger.addEventListener("scrollEnd", () =>
-  scrollToOffset(scrub.vars.offset)
-);
-
-function scrollToOffset(offset) {
-  let snappedTime = snapTime(offset),
-    progress =
-      (snappedTime - seamlessLoop.duration() * iteration) /
-      seamlessLoop.duration(),
-    scroll = progressToScroll(progress);
-  if (progress >= 1 || progress < 0) {
-    return wrap(Math.floor(progress), scroll);
-  }
-  trigger.scroll(scroll);
+function wrapForward(trigger) {
+  iteration++;
+  trigger.wrapping = true;
+  trigger.scroll(trigger.start + 1);
 }
 
-function buildSeamlessLoop(items, spacing, animateFunc) {
-  let rawSequence = gsap.timeline({ paused: true }),
+function wrapBackward(trigger) {
+  iteration--;
+  if (iteration < 0) {
+    iteration = 9;
+    seamlessLoop.totalTime(
+      seamlessLoop.totalTime() + seamlessLoop.duration() * 10
+    );
+    scrub.pause();
+  }
+  trigger.wrapping = true;
+  trigger.scroll(trigger.end - 1);
+}
+
+function buildSeamlessLoop(items, spacing) {
+  let overlap = Math.ceil(1 / spacing),
+    startTime = items.length * spacing + 0.5,
+    loopTime = (items.length + overlap) * spacing + 1,
+    rawSequence = gsap.timeline({ paused: true }),
     seamlessLoop = gsap.timeline({
       paused: true,
       repeat: -1,
       onRepeat() {
         this._time === this._dur && (this._tTime += this._dur - 0.01);
       },
-      onReverseComplete() {
-        this.totalTime(this.rawTime() + this.duration() * 100);
-      },
     }),
-    cycleDuration = spacing * items.length,
-    dur;
+    l = items.length + overlap * 2,
+    time = 0,
+    i,
+    index,
+    item;
 
-  items
-    .concat(items)
-    .concat(items)
-    .forEach((item, i) => {
-      let anim = animateFunc(items[i % items.length]);
-      rawSequence.add(anim, i * spacing);
-      dur || (dur = anim.duration());
-    });
+  gsap.set(items, { xPercent: 400, opacity: 0, scale: 0 });
 
-  seamlessLoop.fromTo(
-    rawSequence,
-    { time: cycleDuration + dur / 2 },
-    { time: "+=" + cycleDuration, duration: cycleDuration, ease: "none" }
-  );
+  for (i = 0; i < l; i++) {
+    index = i % items.length;
+    item = items[index];
+    time = i * spacing;
+    rawSequence
+      .fromTo(
+        item,
+        { scale: 0.3, opacity: 0 },
+        {
+          scale: 1.3,
+          opacity: 1,
+          zIndex: 100,
+          duration: 0.5,
+          yoyo: true,
+          repeat: 1,
+          ease: "power1.in",
+          immediateRender: false,
+        },
+        time
+      )
+      .fromTo(
+        item,
+        { xPercent: 400 },
+        { xPercent: -400, duration: 1, ease: "none", immediateRender: false },
+        time
+      );
+  }
+
+  rawSequence.time(startTime);
+  seamlessLoop
+    .to(rawSequence, {
+      time: loopTime,
+      duration: loopTime - startTime,
+      ease: "none",
+    })
+    .fromTo(
+      rawSequence,
+      { time: overlap * spacing + 1 },
+      {
+        time: startTime,
+        duration: startTime - (overlap * spacing + 1),
+        immediateRender: false,
+        ease: "none",
+      }
+    );
 
   return seamlessLoop;
 }
